6dea6f6f91fe1762275ab10476d632d4a469d033
lazymonk7 <rusti.paula78@gmail.com>
2022-03-12 23:49:12 +0200

    added structure for protocol + server

---
+++ message.c
@@ -0,0 +1,78 @@
+#include "message.h"
+#include <stdlib.h>
+
+header_t *header_constructor1(byte_t mess_type, byte_t mess_subtype, byte_t res_code, unsigned int body_size)
+{
+    header_t *header = malloc(sizeof(header_t));
+    
+    header -> magic[0] = 0x4d; 
+    header -> magic[1] = 0x41, 
+    header -> magic[2] = 0x47;
+    header -> magic[3] = 0x49;
+    header -> magic[4] = 0x43;
+    header -> message_type = mess_type;
+    header -> message_subtype = mess_subtype;
+    header -> response_code = res_code;
+    header -> body_size = body_size;
+
+    return header;
+}
+
+header_t *header_constructor2(byte_t *raw_header)
+{
+    header_t *header;
+    header = deserialize_header(raw_header);
+    return header;
+}
+
+message_t *message_constructor(byte_t *raw_message)
+{
+    message_t *message = malloc(sizeof(message_t));
+    message->body = malloc(MAX_BODY_SIZE*sizeof(byte_t));
+    byte_t raw_header[HEADER_SIZE];
+    
+    for(int i=0; i<HEADER_SIZE; i++)
+    {
+        raw_header[i] = raw_message[i];
+    }
+    message->header = deserialize_header(raw_header);
+    
+    for(int i=0; i<message->header->body_size; i++)
+    {
+        message->body[i] = raw_message[i+HEADER_SIZE];
+    }
+    
+    return message;
+}
+
+byte_t *serialize_header(header_t *header)
+{
+    byte_t *raw_header = malloc(HEADER_SIZE*sizeof(byte_t));
+
+    for(int i=0; i<5; i++)
+    {
+        raw_header[i]=header->magic[i];
+    }
+    raw_header[5]=header->message_type;
+    raw_header[6]=header->message_subtype;
+    raw_header[7]=header->response_code;
+    *(int *)(raw_header+8) = header->body_size;
+
+    return raw_header;
+}
+
+header_t *deserialize_header(byte_t *header_raw)
+{
+    header_t *header = malloc(sizeof(header_t));
+
+    for (int i=0; i<5; i++)
+    {
+        header->magic[i]=header_raw[i];
+    }
+    header->message_type = header_raw[5];
+    header->message_subtype = header_raw[6];
+    header->response_code = header_raw[7];
+    header->body_size = *(int *)(header_raw+8);
+
+    return header;
+}
---
+++ server.h
@@ -0,0 +1,22 @@
+#ifndef server_h
+#define server_h
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BYTES_SIZE_IN_LISTEN 1024 // how many bytes to read from the client process of a peer in order to get a message
+
+typedef struct Server{
+    char *ip_address; //string representing an ip
+    int port;
+    int backlog;
+    struct sockaddr_in address; //socket address
+    int socket;  //basically an open file descriptor
+    void (*start_listening)(struct Server *server, void *(*request_handler)(char *request)); //the fct which spawns new processes for every incoming request
+}server_t;
+
+server_t *server_constructor(char *ip_addr, int port, int backlog);
+
+// util function for reading bytes from the connection in order to get a request message
+void read_request_message(int fd, char *buffer) //reads at most BYTES_SIZE_IN_LISTEN from the given fd and fills the buffer
+#endif
