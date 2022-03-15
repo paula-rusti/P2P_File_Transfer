4d4ba9f9eb965a9e3af244d0c05bd19b75620f1c
mrsaibaandreas <andreasqp@gmail.com>
2022-03-15 22:14:05 +0200

    Removed unnecessary configuration

--- Makefile
+++ Makefile
@@ -2,9 +2,6 @@ CFLAGS+=-Wall
 server: server.c
 	gcc ${CFLAGS} -o c_server server.c 
 
-# utils: utils.c
-# 	gcc ${CFLAGS} -o utils utils.c 
-
 server_conc: server_conc.c
 		gcc ${CFLAGS} -o server_conc server_conc.c 
 
--- server.c
+++ server.c
@@ -4,42 +4,6 @@
 #include<sys/stat.h>
 #include <fcntl.h> 
 
-// struct offset *
-// void segment_file(int file_name, unsigned int nr_of_peers)
-// {
-//     unsigned int nr_of_bytes;
-    
-//     struct stat sb; //only for the member st_size
-
-//     if (fstat(file_name, &sb) == -1) {
-//         perror("stat");
-//         exit(EXIT_FAILURE);
-//     }    
-//     nr_of_bytes = (unsigned int) sb.st_size / nr_of_peers;  //get the neccesary bytes to be transferred by every peer
-    
-//     unsigned int aux = 0;   //stores the last offset 
-//     //struct offset *off_array = malloc(sizeof(struct offset) * nr_of_peers);
-    
-//     for(int index = 0; index < nr_of_peers; index++) {
-//         struct offset temp;
-//         if (aux == 0){
-//             temp.start = aux;   //is valid only for the first peer
-//             temp.end = nr_of_bytes - 1; //because we start from 0
-//         }
-//         else{
-//             temp.start = aux + 1;   //else is end + 1
-//             temp.end = temp.start + nr_of_bytes;
-//         }
-//         aux = temp.end;
-//         //off_array[index] = temp;
-        
-//         printf("peer %d-> start(%u), end(%u)\n", index, temp.start, temp.end);
-//     }
-
-//     //return off_array;
-// }
-
-
 void segment_file(int file_name, unsigned int nr_of_peers) {
 
     unsigned int nr_of_bytes;
@@ -75,6 +39,7 @@ void segment_file(int file_name, unsigned int nr_of_peers) {
             printf("Read %d, bytes_req(%d), bytes_read(%d), buf_size(%u)\n", i, first_time_req, read_bytes, nr_of_bytes);
             
             lseek(file_name_out, 0, SEEK_SET);
+            //here we should call an encryption function or a hash function in order not to send clear data over the network
             write(file_name_out, buf, first_time_req);
             curr_file_offset = read_bytes;
         }
@@ -101,9 +66,9 @@ int main(int argc, char **argv) {
         perror("BAD FILE");
     }
 
-    //unsigned int nr_of_peers = 7;
+    unsigned int nr_of_peers = 7;
     segment_file(file, nr_of_peers);
 
-    //close(file);
+    close(file);
     return 0;
 }
